3dd6827f5646ca27894e29e74ec62bb6b8559c56
Jay Gandhi <Jaygandhi813@gmail.com>
2025-12-04 17:13:40 +0530

    Make 01basic folder and put 1st and conversionOperation

---
+++ 01Basics/02_ConversionOperation.js
@@ -0,0 +1,72 @@
+let score = "77";
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(valueInNumber);
+console.log(typeof (valueInNumber));
+
+// "77" => 33
+// "77abc" => NaN (Not a Number )
+// true => 1 / false => 0
+// undefine => NaN
+
+console.log("???????");
+
+
+let jayWonMatch = 1;
+console.log(typeof (jayWonMatch));
+
+let booleanIsWon = Boolean(jayWonMatch);
+console.log(typeof (booleanIsWon));
+console.log(booleanIsWon);
+
+// 1 => true; 0 => flase
+// "" => false
+// "Jay" => true
+
+let someNumber = 73
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber); => 73
+// console.log(typeof stringNumber); => String
+
+// ***** Operations *****
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);  =>  4
+// console.log(2-2);  =>  0
+// console.log(2*2);  =>  4
+// console.log(2**3);  =>  8
+// console.log(2/3);  =>  0.666666666
+// console.log(2%3);  =>  2
+
+let str1 = "hello"
+let str2 = " Jay"
+
+let str3 = str1 + str2
+// console.log(str3);  =>  hello Jay
+
+// console.log("1" + 2);  =>  12
+// console.log(1 + "2");  =>  12
+// console.log("1" + 2 + 2);  =>  122
+// console.log(1 + 2 + "2");  =>  32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let counter = 100
+++counter;
+console.log(counter);
+
+// prefix and postfix 
